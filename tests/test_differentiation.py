dcb87a26581506b8a3fa3266c524cd40b6aeda28
sanskarpan <sanskarpandey2004@gmail.com>
2026-04-14 17:35:19 +0530

    Remove unnecessary imports from differentiation unit tests for cleaner code

--- tests/test_differentiation.py
+++ tests/test_differentiation.py
@@ -2,10 +2,6 @@
 Unit tests for differentiation module
 """
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
 import unittest
 import math
 from Calculus.differentiation import (
